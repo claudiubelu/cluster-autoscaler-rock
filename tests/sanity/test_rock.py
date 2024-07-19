964ecfcf1a815305dae1c8f8b72e540793dc62a9
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-19 09:48:48 +0000

    Adds 1.27.2 rock

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -1,16 +1,23 @@
-import subprocess
-import os
+from k8s_test_harness.util import docker_util, env_util
 
 
-def test_sanity():
-    image_variable = "ROCK_CLUSTER_AUTOSCALER"
-    entrypoint = "/cluster-autoscaler"
-    image = os.getenv(image_variable)
-    assert image is not None, f"${image_variable} is not set"
-    # assert we have the expected files
-    docker_run = subprocess.run(
-        ["docker", "run", "--rm", "--entrypoint", entrypoint, image, "--help"],
-        capture_output=True,
-        text=True,
+def _test_sanity(image_version):
+    rock = env_util.get_build_meta_info_for_rock_version(
+        "cluster-autoscaler", image_version, "amd64"
     )
-    assert "Usage of /cluster-autoscaler:" in docker_run.stderr
+    image = rock.image
+
+    app_name = "cluster-autoscaler"
+    entrypoint = f"/{app_name}"
+
+    # assert we have the expected files
+    process = docker_util.run_in_docker(image, [entrypoint, "version"])
+    assert app_name in process.stdout and image_version in process.stdout
+
+
+def test_cluster_autoscaler_1_24_0():
+    _test_sanity("1.24.0")
+
+
+def test_cluster_autoscaler_1_27_2():
+    _test_sanity("1.27.2")
