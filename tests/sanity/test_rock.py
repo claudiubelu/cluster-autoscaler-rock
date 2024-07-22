7bcdd51d707e12bab951130f516ad41770fd8184
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-22 08:47:29 +0000

    Parameterize sanity test

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -5,9 +5,11 @@
 import subprocess
 
 from k8s_test_harness.util import env_util
+import pytest
 
 
-def _test_sanity(image_version):
+@pytest.mark.parametrize("image_version", ("1.24.0", "1.27.2"))
+def test_sanity(image_version):
     rock = env_util.get_build_meta_info_for_rock_version(
         "cluster-autoscaler", image_version, "amd64"
     )
@@ -21,11 +23,3 @@ def _test_sanity(image_version):
         text=True,
     )
     assert "Usage of /cluster-autoscaler:" in docker_run.stderr
-
-
-def test_cluster_autoscaler_1_24_0():
-    _test_sanity("1.24.0")
-
-
-def test_cluster_autoscaler_1_27_2():
-    _test_sanity("1.27.2")
